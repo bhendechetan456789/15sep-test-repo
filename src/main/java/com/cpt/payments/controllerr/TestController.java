8f23020b00c4a5e22db58bab29e087d4f0e2a63b
Chetan Bhende <chetanBhende1996@gmail.com>
2023-09-25 21:32:37 +0530

    Added getWorld method

--- src/main/java/com/cpt/payments/controllerr/TestController.java
+++ src/main/java/com/cpt/payments/controllerr/TestController.java
@@ -1,5 +1,19 @@
 package com.cpt.payments.controllerr;
 
-public class TestController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class TestController {
+   
+	@GetMapping("/helloworld")
+	public String helloworld() {
+		return "helloworld";
+		
+	}
+	@GetMapping("/getworld")
+	public String getworld() {
+		return "Get world";
+	
+}
 }
